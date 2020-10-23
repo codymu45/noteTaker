7132f602f5041de7026f1ca8bc8a35cedc618b3b
Cody Mullen <mullen.cody@yahoo.com>
2020-10-22 19:19:26 -0700

    trying to fix server

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Root URL
-app.use( express.static("/"); )
+app.use( express.static("/"));
 
 app.get('/index.html', (req, res) =>{
     res.sendFile('index.html')
